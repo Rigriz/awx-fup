406eb61cfc7e0a41f0663c9b0a31b0bfcd2e3c9b
Rigriz <rigriz@gmail.com>
2026-04-09 09:51:35 +0530

    initial commit

---
+++ inventory/ip_inventory.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import json
+import sys
+
+API_URL = "http://localhost:8090/regester"
+
+# You can pass lab name as argument OR hardcode
+LAB_NAME = "411C"
+
+response = requests.get(API_URL)
+data = response.json()
+
+lab_data = data.get(LAB_NAME, {})
+
+inventory = {
+    "all": {"hosts": []},
+    "_meta": {"hostvars": {}}
+}
+
+for side in lab_data:   # L, R
+    inventory[side] = {"hosts": []}
+
+    for device_name, details in lab_data[side].items():
+        ip = details["ip"]
+
+        inventory["all"]["hosts"].append(device_name)
+        inventory[side]["hosts"].append(device_name)
+
+        inventory["_meta"]["hostvars"][device_name] = {
+             "ansible_host": ip,
+             "ansible_user": "bmsit",
+             "ansible_password": "123456",
+             "ansible_ssh_common_args": "-o StrictHostKeyChecking=no"
+         }
+
+print(json.dumps(inventory))
